969e2c529521483dd3f8aec8d7623edac656194e
Piyush Shaw <piyushshaw8777@gmail.com>
2021-06-30 00:23:11 +0530

    Added : write or read a .txt file

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,243 @@
+/**
+ * ************************** 
+ * Purpose: To Read or Write the Address Book with Persons Contact into a 
+ *              File using File IO
+ *                      
+ * @author Piyush Shaw
+ * @version 1.0
+ * @since 28-06-2021
+ * **************************
+ */
+
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class AddressBook {
+	public enum IOService{CONSOLE_IO, FILE_IO, DATABASE_IO,  REST_IO}
+
+    Scanner scanner = new Scanner(System.in);
+    List<Contact> contactList = new ArrayList<>();
+    AddressBookManager addressBookManager = new AddressBookManager();
+
+
+    //@createAddBook --> it creates a addressbook with a user input name
+    public void createAddBook() {
+        addressBookManager.createAddressBook();
+    }
+    //this Method is used to get the contacts from the user entered AddressBook..
+
+
+    // To add the contacts in addressBook and returns the list of contacts
+    public List<Contact> addContacts() {
+        System.out.println("Add new Contact to ADDRESS BOOK:--");
+        System.out.println("Available address books are: "+ addressBookManager.addressBookMap.keySet());
+
+        System.out.println("Enter the AddressBook Name to which you want to add contacts: ");
+        String addressBookName = scanner.next();
+        System.out.println("enter the number of people you want to add in addressbook: ");
+        int numberOfUser =scanner.nextInt();
+
+        for (int i = 0; i < numberOfUser; i++) {
+            Contact person = new Contact();
+            System.out.println("First Name: ");
+            String firstName = scanner.next();
+            person.setFirstName(firstName);
+            if (checkDuplicate(firstName)) {
+                System.out.println("Person name already exist");
+            } else {
+
+                System.out.println("Last Name: ");
+                String lastName = scanner.next();
+                person.setLastName(lastName);
+
+                System.out.println("Address: ");
+                String address = scanner.next();
+                person.setAddress(address);
+
+                System.out.println("City: ");
+                String city = scanner.next();
+                person.setCity(city);
+
+                System.out.println("State: ");
+                String state = scanner.next();
+                person.setState(state);
+
+                System.out.println("EmailID: ");
+                String email = scanner.next();
+                person.setEmail(email);
+
+                System.out.println("Zip: ");
+                int zip = scanner.nextInt();
+                person.setZip(zip);
+
+                System.out.println("PhoneNumber: ");
+                Long phoneNo = scanner.nextLong();
+                person.setPhoneNumber(phoneNo);
+
+                addressBookManager.checkAddressBookandaddContact(addressBookName, person);
+                Contact contact = new Contact(firstName, lastName, address, city, state, email,
+                        zip, phoneNo);
+                contactList.add(person);
+
+            }
+        }
+        return contactList;
+
+    }
+    // @edit is used to edit the contact details of the user
+
+    public List<Contact> edit() {
+        System.out.println("Enter the AddressBook Name to which you want to edit contacts: ");
+        String addressBookName = scanner.next();
+
+        System.out.println("Editing a contact");
+        System.out.println("Enter the name of contact you want to edit: ");
+        Scanner input = new Scanner(System.in);
+        String editContact = input.next();
+        for (Contact editPerson : contactList) {
+            if (editPerson.getFirstName().equals(editContact)) {
+                System.out.println("which field you want to edit :" + "\n" + "[1] : Edit firstName" + "\n" + "[2] : Edit LastName" + "\n" + "[3]: Edit Address" + "\n" + "[4] : Edit City " + "\n" + "[5]: Enter zipCode" + "\n" + "[6]: Enter MobileNo" + "\n" + "[7]: Enter EmailId");
+                switch (input.nextInt()) {
+                    case 1:
+                        System.out.println("Change the First Name: ");
+                        String editfName = input.next();
+                        editPerson.setFirstName(editfName);
+                        break;
+                    case 2:
+                        System.out.println("Change the Last Name: ");
+                        String editlName = input.next();
+                        editPerson.setLastName(editlName);
+                        break;
+                    case 3:
+                        System.out.println("Change the Address: ");
+                        String editAddress = input.next();
+                        editPerson.setAddress(editAddress);
+                        break;
+                    case 4:
+                        System.out.println("Change the City: ");
+                        String editCity = input.next();
+                        editPerson.setCity(editCity);
+                        break;
+                    case 5:
+                        System.out.println("Change the zip Code: ");
+                        int editZipCode = input.nextInt();
+                        editPerson.setZip(editZipCode);
+                        break;
+                    case 6:
+                        System.out.println("Change the Phone Number: ");
+                        Long editPhoneNum = input.nextLong();
+                        editPerson.setPhoneNumber(editPhoneNum);
+                        break;
+                    case 7:
+                        System.out.println("Change the Email ID: ");
+                        String editEmail = input.next();
+                        editPerson.setEmail(editEmail);
+                        break;
+                }
+                addressBookManager.editContact(addressBookName, editPerson);
+                contactList.add(editPerson);
+
+            }
+        }
+        return contactList;
+    }
+    // @deleteContact is used to delete the contact in a adddressbook
+    public boolean deleteContact(String name) {
+        int flag = 0;
+        for (Contact contact : contactList) {
+            if (contact.getFirstName().equals(name)) {
+                contactList.remove(contact);
+                System.out.println("Contact left are " + contactList);
+                flag = 1;
+                break;
+            }
+        }
+        return flag == 1;
+    }
+    // @CheckDuplicat  restricts the user to enter the same first name again in a particular address book .
+    public boolean checkDuplicate(String fname) {
+        int flag = 0;
+        for (Contact p : contactList) {
+            if (p.getFirstName().equals(fname)) {
+                flag = 1;
+                break;
+            }
+        }
+        return flag == 1;
+    }
+    // used to filter firstname and lastname of the contact belonging to same state .
+    public void getPersonNameByState(String State) {
+        List<Contact> details = contactList.stream().filter(contactName -> contactName.getState().equals(State))
+                .collect(Collectors.toList());
+        for (Contact contact : details) {
+            for (Entry<String, LinkedList> entry : addressBookManager.addressBookMap.entrySet()) {
+                LinkedList value = entry.getValue();
+                System.out.println("The Address Book: " + entry.getKey());
+            }
+            System.out.println("First Name: " + contact.getFirstName());
+            System.out.println("Last Name: " + contact.getLastName());
+        }
+    }
+    // used to filter firstname and lastname of the contact belonging to same city .
+    public void getPersonNameByCity(String cityName) {
+        List<Contact> details2 = contactList.stream().filter(contactName -> contactName.getCity().equals(cityName))
+                .collect(Collectors.toList());
+        for (Contact contact : details2) {
+            for (Map.Entry<String, LinkedList> entry : addressBookManager.addressBookMap.entrySet()) {
+                LinkedList value = entry.getValue();
+                System.out.println("The Address Book: " + entry.getKey());
+            }
+            System.out.println("First Name: " + contact.getFirstName());
+            System.out.println("Last Name: " + contact.getLastName());
+        }
+    }
+    public void countByCity(String city) {
+        int countPersonInCity = 0;
+        for (Map.Entry<String, LinkedList> entry : addressBookManager.addressBookMap.entrySet()) {
+            for (int index = 0; index < contactList.size(); index++){
+                if (contactList.get(index).getCity().equals(city)){
+                    System.out.println(contactList.get(index) );
+                    countPersonInCity = index;
+                }
+            }
+        }
+        System.out.println("Total number of people in this city " + city + ": " + countPersonInCity);
+    }
+    public void getContacts() {
+        System.out.println("From which addressbook you want to find details?? ");
+        String addressBookName = scanner.next();
+        addressBookManager.getContactByAddressBook(addressBookName);
+    }
+    public void sortByName() {
+        System.out.println("From which addressbook you want to find details?? ");
+        String addressBookName = scanner.next();
+        System.out.println("AddressBook name:" +addressBookName);
+        List<Contact> sortedList= addressBookManager.addressBookMap.get(addressBookName);
+        contactList.stream().sorted(Comparator.comparingInt(Contact::getZip)).collect(Collectors.toList());
+        sortedList.forEach(System.out::println);
+    }
+    public void sortByZip() {
+        System.out.println("From which addressbook you want to find details?? ");
+        String addressBookName = scanner.next();
+        System.out.println("AddressBook name:" +addressBookName);
+        List<Contact> contactList= addressBookManager.addressBookMap.get(addressBookName);
+        contactList.stream().sorted(Comparator.comparing(Contact::getFirstName)).collect(Collectors.toList());
+        contactList.forEach(System.out::println);
+    }
+
+    public void writeContactPersonData(IOService ioService) {
+        if(ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("CONSOLE: " +contactList);
+        else if(ioService.equals(IOService.FILE_IO))
+            addressBookManager.writeDataInFile();
+    }
+
+}
---
+++ src/com/bridgelabz/AddressBookFileIOService.java
@@ -0,0 +1,45 @@
+package com.bridgelabz;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBookFileIOService {
+	 public void writeData(List<Contact> contactList , String addressBookName) {
+	        StringBuffer personBuffer = new StringBuffer();
+	        contactList.forEach( person-> {
+	            String contactDataString = person.toString().concat("\n");
+	            personBuffer.append(contactDataString);
+
+	        });
+	        try {
+	            Files.write(Paths.get(addressBookName + ".txt"),personBuffer.toString().getBytes());
+	        }catch (IOException e){
+	            e.printStackTrace();
+	        }
+	    }
+
+	    public static List<Contact> readData (){
+	        List<Contact> contactPersonList = new ArrayList<>();
+	        try {
+	            Files.lines(new File("chattisgarh.txt").toPath())
+	                    .map(line -> line.trim())
+	                    .forEach(line->System.out.println(line));
+	        }catch (IOException e){
+	            e.printStackTrace();
+	        }
+	        return contactPersonList;
+	    }
+	    public void printData(){
+	        try {
+	            Files.lines(new File("chattisgarh.txt").toPath())
+	                    .forEach(System.out::println);
+	        }catch (IOException e){
+	            e.printStackTrace();
+	        }
+	    }
+
+}
